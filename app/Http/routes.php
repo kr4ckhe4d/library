59dd3b07f4c2e6460ba6d58f52033548620cce48
nilesh93 <nilesh.jayanandana@yahoo.com>
2015-08-29 16:02:02 +0530

    fixed fucking search

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,7 @@ Route::get('insertBooks', function(){
 
 
 
-return view('insertBooks');
+    return view('insertBooks');
 
 
 });
@@ -32,38 +32,38 @@ Route::get('/addBook', function(){
     $author = Request::input('author');
     $cl_num = Request::input('cl_num');
     $edit = Request::input('edit');
-    
-    
+
+
     $publisher = Request::input('publisher');
     $published_place = Request::input('published_place');
     $published_year = Request::input('published_year');
-    
-    
-    
+
+
+
     $pages = Request::input('pages');
     $height = Request::input('height');
-     
-    
-    
+
+
+
     $series = Request::input('series');
     $series_num = Request::input('series_num');
     $isbn = Request::input('isbn');
     $remarks = Request::input('remarks');
-     
-    
+
+
     $date = date("Y-m-d");
-    
- $id = DB::table('book_names')->insertGetId(
-      
-     
-     
-     array('name' => $name, 'author' => $author, 'cl_number' => $cl_num, 'edit_translate' => $edit, 'publisher' => $publisher, 'published_place' => $published_place, 'published_year' => $published_year, 'pages' => $pages, 'height' => $height, 'series' => $series, 'series_num' => $series_num, 'isbn' => $isbn, 'remarks' => $remarks, 'date_created'=> $date, 'date_updated' => $date)
-);
-    
-    
-    
-    
-return $id;
+
+    $id = DB::table('book_names')->insertGetId(
+
+
+
+        array('name' => $name, 'author' => $author, 'cl_number' => $cl_num, 'edit_translate' => $edit, 'publisher' => $publisher, 'published_place' => $published_place, 'published_year' => $published_year, 'pages' => $pages, 'height' => $height, 'series' => $series, 'series_num' => $series_num, 'isbn' => $isbn, 'remarks' => $remarks, 'date_created'=> $date, 'date_updated' => $date)
+    );
+
+
+
+
+    return $id;
 
 
 });
@@ -77,39 +77,39 @@ Route::get('/updateBook', function(){
     $author = Request::input('author');
     $cl_num = Request::input('cl_num');
     $edit = Request::input('edit');
-    
-    
+
+
     $publisher = Request::input('publisher');
     $published_place = Request::input('published_place');
     $published_year = Request::input('published_year');
-    
-    
-    
+
+
+
     $pages = Request::input('pages');
     $height = Request::input('height');
-     
-    
-    
+
+
+
     $series = Request::input('series');
     $series_num = Request::input('series_num');
     $isbn = Request::input('isbn');
     $remarks = Request::input('remarks');
-     
-    
+
+
     $date = date("Y-m-d");
-    
+
     $bnid = Request::input('bnid');
-      
-     
-     
-    
+
+
+
+
     $book_name = DB::table('book_names')
         ->where('id', '=', $bnid)
         ->update(array('name' => $name, 'author' => $author, 'cl_number' => $cl_num, 'edit_translate' => $edit, 'publisher' => $publisher, 'published_place' => $published_place, 'published_year' => $published_year, 'pages' => $pages, 'height' => $height, 'series' => $series, 'series_num' => $series_num, 'isbn' => $isbn, 'remarks' => $remarks, 'date_updated' => $date));
-    
-    
-    
-return 'success';
+
+
+
+    return 'success';
 
 
 });
@@ -140,14 +140,14 @@ Route::get('/addCopy', function(){
     $copydate = Request::input('copydate');
     $copyremarks = Request::input('copyremarks');
     $date = date("Y-m-d");
-    
- $id = DB::table('books')->insertGetId(array('book_name_id' => $bnid, 'remarks' => $copyremarks, 'id' =>  $cid,   'date_added'=> $date, 'date_updated' => $date)
-);
+
+    $id = DB::table('books')->insertGetId(array('book_name_id' => $bnid, 'remarks' => $copyremarks, 'id' =>  $cid,   'date_added'=> $date, 'date_updated' => $date)
+                                         );
     $book = DB::table('books')->where('book_name_id','=',$bnid)->orderBy('id')->get();
-    
-    
-    
-return $book;
+
+
+
+    return $book;
 
 
 });
@@ -158,23 +158,23 @@ Route::get('/deleteCopy', function(){
 
     $bnid = Request::input('bnid');
     $cid = Request::input('cid');
- 
-    
-   
-   
-    
-    
-     
-     
-    
-     
-    
+
+
+
+
+
+
+
+
+
+
+
     DB::table('books')->where('id','=',$cid)->delete();
     $book = DB::table('books')->where('book_name_id','=',$bnid)->orderBy('id')->get();
-    
-    
-    
-return $book;
+
+
+
+    return $book;
 
 
 });
@@ -183,7 +183,7 @@ return $book;
 
 
 
- 
+
 
 
 
@@ -205,8 +205,8 @@ return $book;
 
 
 Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
 ]);
 
 
@@ -231,54 +231,56 @@ Route::get('searchBooks', function(){
 
 
 
-return view('searchBooks');
+    return view('searchBooks');
 
 
 });
 
 Route::get('searchBooks1', function(){
-  
+
     $search = Input::get('search');
-     $select = Input::get('select');
-    
-  // $bookname = DB::table('book_names AS a')
-   //    ->leftJoin('books AS b', function($join)
-   //        {
-//$join->on( 'a.id', '=', 'b.book_name_id')
-   //           ->where('b.status', '=', '0');
-//
-   //        })
-//->select(DB::raw('count(b.id) as available, a.*'))
-   //   ->where($select, 'LIKE', $search.'%')
-   //    ->groupBy('a.id')
-//->orderBy($select)
-   //    ->get();
-	//	 
- 
-       $bookname=  DB::table('book_names')
-             ->leftJoin('books', 'book_names.id','=','books.book_name_id')
-         
-            
-            ->groupBy('book_names.id')
-            ->get();
-    
-   
-       
- return   $bookname;
+    $select = Input::get('select');
+
+    // $bookname = DB::table('book_names AS a')
+    //    ->leftJoin('books AS b', function($join)
+    //        {
+    //$join->on( 'a.id', '=', 'b.book_name_id')
+    //           ->where('b.status', '=', '0');
+    //
+    //        })
+    //->select(DB::raw('count(b.id) as available, a.*'))
+    //   ->where($select, 'LIKE', $search.'%')
+    //    ->groupBy('a.id')
+    //->orderBy($select)
+    //    ->get();
+    //	 
+
+    $bookname=  DB::table('book_names')
+        ->leftJoin('books', 'book_names.id','=','books.book_name_id')
+
+        ->select(DB::raw('count(books.id) as available, book_names.*'))
+        ->where('book_names.'.$select, 'LIKE', $search.'%')
+        ->groupBy('book_names.id')
+        ->orderBy('book_names.'.$select)
+        ->get();
+
+
+
+    return   $bookname;
 
 });
 
 
 
- Route::get('/single-book', function(){
-  $id = Input::get('id');
+Route::get('/single-book', function(){
+    $id = Input::get('id');
 
-     
-     $book_name = DB::table('book_names')->where('id','=', $id)->first();
-     $book = DB::table('books')->where('book_name_id','=', $id)->orderBy('id')->get();
 
-return view('single-book')
-       ->with("book_name", $book_name)
+    $book_name = DB::table('book_names')->where('id','=', $id)->first();
+    $book = DB::table('books')->where('book_name_id','=', $id)->orderBy('id')->get();
+
+    return view('single-book')
+        ->with("book_name", $book_name)
         ->with("book", $book);
 
 
@@ -300,7 +302,7 @@ Route::get('/single-copy', function(){
 
 
 
-return view('single-copy');
+    return view('single-copy');
 
 
 });
--- resources/views/searchBooks.blade.php
+++ resources/views/searchBooks.blade.php
@@ -17,10 +17,10 @@ SEARCH BOOKS
 
 
 <li><a href="#">Books</a>
-                        </li>
-                        <li class="active">
-                            <strong>SearchBooks</strong>
-                        </li>
+</li>
+<li class="active">
+    <strong>SearchBooks</strong>
+</li>
 
 
 @endsection
@@ -33,14 +33,14 @@ SEARCH BOOKS
 
 
 
-      <li class="active">
-                    <a href="#"><i class="fa fa-book"></i> <span class="nav-label">Books</span> </a>
-                    <ul class="nav nav-second-level">
-                        <li class="active" ><a href="searchBooks">Search Books</a></li>
-                        <li ><a href="insertBooks">Insert Books</a></li>
- 
-                    </ul>
-                </li>
+<li class="active">
+    <a href="#"><i class="fa fa-book"></i> <span class="nav-label">Books</span> </a>
+    <ul class="nav nav-second-level">
+        <li class="active" ><a href="searchBooks">Search Books</a></li>
+        <li ><a href="insertBooks">Insert Books</a></li>
+
+    </ul>
+</li>
 
 
 
@@ -66,193 +66,193 @@ SEARCH BOOKS
 
 
 @section('content')
-<div class="container" >
-   <br>
-	<div class="row">
-    
-		<form class="form-horizontal">
-      
- 
-    <div class="form-group">
-      
-        
-        <div class="col-lg-3">
-        <input class="form-control"   oninput="searchf()" id="search" placeholder="Enter Keyword" type="text">
-            </div>
-        
-        
-          <div class="col-lg-3" style="padding-left:0cm;">
-      <select class="form-control" id="selectsearch" onchange="searchf()">
-          <option value="author" selected>Author</option>
-          <option  value="name">Name</option>
-          <option value="cl_number">Cl Number</option>
-          <option value="publisher">Publisher</option>
-          <option value="edit_translate">Editor/Translator</option>
-          <option value="series">Series</option>
-        </select>
-            </div>
-        <div class="col-lg-1" style="padding-left:0cm;">
-       <button type="button" class="btn btn-primary btn-block" onclick="searchf()">Search</button>
-            </div>
-        
-      </div>
-            
-            
-            
-       
-            
-         
-        </form> 
-	</div>
+<div class="container" style="width:99%" >
     <br>
     <div class="row">
-     
-    
-    
-    <table class="table table-hover table-striped">
-        
-        <thead>
-        <th class="col-lg-3"> Name</th>
-        <th class="col-lg-2">Author</th>
-        <th class="col-lg-1">Cl Number</th>
-         
-        <th class="col-lg-1">Editor/Translator</th>
-        <th class="col-lg-2">Series</th>
-        <th class="col-lg-1">Series No.</th>
-         
-         <th class="col-lg-1">Availabale</th>   
-            <th class="col-lg-1"></th>    
-            
-        </thead>
-        
-        <tbody id="searchbody">
-        
-        
-        
-        
-        
-        
-        </tbody>
-       
-        
-        
-        </table>
-    
-    
-    
-    
-    
+
+        <form class="form-horizontal">
+
+
+            <div class="form-group">
+
+
+                <div class="col-lg-3">
+                    <input class="form-control"   oninput="searchf()" id="search" placeholder="Enter Keyword" type="text">
+                </div>
+
+
+                <div class="col-lg-3" style="padding-left:0cm;">
+                    <select class="form-control" id="selectsearch" onchange="searchf()">
+                        <option value="author" selected>Author</option>
+                        <option  value="name">Name</option>
+                        <option value="cl_number">Cl Number</option>
+                        <option value="publisher">Publisher</option>
+                        <option value="edit_translate">Editor/Translator</option>
+                        <option value="series">Series</option>
+                    </select>
+                </div>
+                <div class="col-lg-1" style="padding-left:0cm;">
+                    <button type="button" class="btn btn-primary btn-block" onclick="searchf()">Search</button>
+                </div>
+
+            </div>
+
+
+
+
+
+
+        </form> 
     </div>
-    
-    
-    
-    
+    <br>
+    <div class="row">
+
+
+
+        <table class="table table-hover table-striped">
+
+            <thead>
+                <th class="col-md-3"> Name</th>
+                <th class="col-md-2">Author</th>
+                <th class="col-md-1">Cl Number</th>
+
+                <th class="col-md-1">Editor/Translator</th>
+                <th class="col-md-2">Series</th>
+                <th class="col-md-1">Series No.</th>
+
+                <th class="col-md-1">Availabale</th>   
+                <th class="col-md-1"></th>    
+
+            </thead>
+
+            <tbody id="searchbody">
+
+
+
+
+
+
+            </tbody>
+
+
+
+        </table>
+
+
+
+
+
+    </div>
+
+
+
+
 </div>
 
- 
+
 
 
 
 
 
 <script>
-    
-     
-     function reset(){
-     
-     
-     
-     
-      var search = '';   
- var select = 'name';
-  
- var data = 'search='+search+'&select='+select;
-      $.ajax({
-    type: "get",
-    url: '/library/public/searchBooks1',
-    data: data,
-        
-    success : function(data){
-       
-        var body ="";
-        
-        
-        for(var i=0;i<data.length; i++){
-        
-        body+= '<tr> <td style="font-weight:400; font-size:110%"><a style="color:black;" href="/library/public/single-book?id='+data[i].id+'">'+ data[i].name +'</a></td> <td style="font-weight:400; font-size:110%">'+ data[i].author +'</td> <td style="font-weight:400; font-size:110%"><center>'+ data[i].cl_number+'</center></td>   <td style="font-weight:400; font-size:110%">'+data[i].edit_translate+'</td> <td style="font-weight:400; font-size:110%">'+data[i].series+'</td> <td style="font-weight:400; font-size:110%"> <center>'+data[i].series_num+'</center></td> <td style="font-weight:400; font-size:110%"><center>'+ data[i].available+'</center></td> <td><a href="/library/public/single-book?id='+data[i].id+'" class="btn btn-primary btn-block">View</a> </td></tr>';
-        
-        
-        
-        
-        
-        
-        
-        }
-        
-        document.getElementById("searchbody").innerHTML = body;
-        
-         
-        
-        },
-	 error: function(xhr, ajaxOptions, thrownError) {
-        alert("error");
-      }	 
-    });
- 
-     
-     
-     
-     
-     
-     
-     
-     }
-    
-    
- function searchf(){
-  
-  var search = $('#search').val();   
- var select = $('#selectsearch').val();
-  
- var data = 'search='+search+'&select='+select;
-      $.ajax({
-    type: "post",
-    url: '/library/public/searchBooks1',
-    data: data,
-        
-    success : function(data){
-       
-        var body ="";
-        
-        
-        for(var i=0;i<data.length; i++){
-        
-      body+= '<tr> <td style="font-weight:400; font-size:110%"><a style="color:black;" href="/library/public/single-book?id='+data[i].id+'">'+ data[i].name +'</a></td> <td style="font-weight:400; font-size:110%">'+ data[i].author +'</td> <td style="font-weight:400; font-size:110%"><center>'+ data[i].cl_number+'</center></td>   <td style="font-weight:400; font-size:110%">'+data[i].edit_translate+'</td> <td style="font-weight:400; font-size:110%">'+data[i].series+'</td> <td style="font-weight:400; font-size:110%"> <center>'+data[i].series_num+'</center></td> <td style="font-weight:400; font-size:110%"><center>'+ data[i].available+'</center></td> <td><a href="/library/public/single-book?id='+data[i].id+'" class="btn btn-primary btn-block">View</a> </td></tr>';
-        
-        
-        
-        
-        
-        
-        
-        }
-        
-        document.getElementById("searchbody").innerHTML = body;
-        
-         
-        
-        },
-	 error: function(xhr, ajaxOptions, thrownError) {
-        alert("error");
-      }	 
-    });
- 
-     
-     
-     
-     
-     
- }
+
+
+    function reset(){
+
+
+
+
+        var search = '';   
+        var select = 'name';
+
+        var data = 'search='+search+'&select='+select;
+        $.ajax({
+            type: "get",
+            url: '/library/public/searchBooks1',
+            data: data,
+
+            success : function(data){
+
+                var body ="";
+
+
+                for(var i=0;i<data.length; i++){
+
+                    body+= '<tr> <td style="font-weight:400; font-size:110%"><a style="color:black;" href="/library/public/single-book?id='+data[i].id+'">'+ data[i].name +'</a></td> <td style="font-weight:400; font-size:110%">'+ data[i].author +'</td> <td style="font-weight:400; font-size:110%"><center>'+ data[i].cl_number+'</center></td>   <td style="font-weight:400; font-size:110%">'+data[i].edit_translate+'</td> <td style="font-weight:400; font-size:110%">'+data[i].series+'</td> <td style="font-weight:400; font-size:110%"> <center>'+data[i].series_num+'</center></td> <td style="font-weight:400; font-size:110%"><center>'+ data[i].available+'</center></td> <td><a href="/library/public/single-book?id='+data[i].id+'" class="btn btn-primary btn-block">View</a> </td></tr>';
+
+
+
+
+
+
+
+                }
+
+                document.getElementById("searchbody").innerHTML = body;
+
+
+
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert("error");
+            }	 
+        });
+
+
+
+
+
+
+
+
+    }
+
+
+    function searchf(){
+
+        var search = $('#search').val();   
+        var select = $('#selectsearch').val();
+
+        var data = 'search='+search+'&select='+select;
+        $.ajax({
+            type: "get",
+            url: '/library/public/searchBooks1',
+            data: data,
+
+            success : function(data){
+
+                var body ="";
+
+
+                for(var i=0;i<data.length; i++){
+
+                    body+= '<tr> <td style="font-weight:400; font-size:110%"><a style="color:black;" href="/library/public/single-book?id='+data[i].id+'">'+ data[i].name +'</a></td> <td style="font-weight:400; font-size:110%">'+ data[i].author +'</td> <td style="font-weight:400; font-size:110%"><center>'+ data[i].cl_number+'</center></td>   <td style="font-weight:400; font-size:110%">'+data[i].edit_translate+'</td> <td style="font-weight:400; font-size:110%">'+data[i].series+'</td> <td style="font-weight:400; font-size:110%"> <center>'+data[i].series_num+'</center></td> <td style="font-weight:400; font-size:110%"><center>'+ data[i].available+'</center></td> <td><a href="/library/public/single-book?id='+data[i].id+'" class="btn btn-primary btn-block">View</a> </td></tr>';
+
+
+
+
+
+
+
+                }
+
+                document.getElementById("searchbody").innerHTML = body;
+
+
+
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert("error");
+            }	 
+        });
+
+
+
+
+
+
+    }
 </script>
 
 
